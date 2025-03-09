1c000b8ebe209d9ca6072e10facdd93b90506906
Siddhanth Ramani <dhanth20@gmail.com>
2025-03-08 18:56:56 -0800

    Bug: Changed Max Date = 202-06-06

--- DataCleaning_Stanford.py
+++ DataCleaning_Stanford.py
@@ -8,7 +8,7 @@ useful_cols = ['index_number', 'composite_litigation_name', 'company_ticker_symb
        'issuer_comp_citizen_jurisdict', 'cld_fic_court_usdc_luv',
        'company_market', 'company_market_status', 'cld_case_status_gen_luv', 'cld_flag_case_ever_go_trial', 'case_last_stage_date']
 df_sub = df[useful_cols]
-df_sub = df_sub[(df_sub["cld_fic_class_start_dt"] <"2020-06-09") 
+df_sub = df_sub[(df_sub["cld_fic_class_start_dt"] <="2020-06-09") 
                 & (df_sub["cld_fic_class_start_dt"] >="2010-01-01")].reset_index(drop=True)
 df_sub["company_ticker_symbol"] = df_sub["company_ticker_symbol"].str.strip()
 df_sub["composite_litigation_name"] = df_sub["composite_litigation_name"].str.strip()
